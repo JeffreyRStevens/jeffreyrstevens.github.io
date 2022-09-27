c92259af0ddec093e6db6bd2e6c6e9b9c92c1989
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2022-09-27 17:30:12 -0500

    Flip margin for asides in CV

--- R/extract_pubs.R
+++ R/extract_pubs.R
@@ -4,7 +4,7 @@ startline <- which(cvfile == "## Publications")
 endline <- which(cvfile == "## Presentations")
 body <- cvfile[(startline+1):(endline-2)]
 
-prelim <- c("---", 'title: "Publications"', 'format:', '  html:', "    toc: true", "    toc-location: left", "---")
+prelim <- c("---", 'title: "Publications"', 'format:', '  html:', "    css: flipmargin.css", "    toc: true", "    toc-location: left", "---")
 
 newfile <- c(prelim, body)
 writeLines(newfile, "publications.qmd")
